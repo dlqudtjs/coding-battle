9a05b964023f3e4476f34eece779313c144bab7b
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-24 14:53:58 +0900

    first commit

---
+++ src/main/java/com/dlqudtjs/codingbattle/CodingbattleApplication.java
@@ -0,0 +1,12 @@
+package com.dlqudtjs.codingbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingbattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingbattleApplication.class, args);
+	}
+}
