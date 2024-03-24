70680d4ca2ec3cd6784c1435389e453cdf0cae1c
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-24 18:02:56 +0900

    feat - logging 기능 추가

---
+++ src/main/java/com/dlqudtjs/codingbattle/configuration/log/LogConfig.java
@@ -0,0 +1,43 @@
+package com.dlqudtjs.codingbattle.configuration.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class LogConfig {
+
+    @Pointcut("execution(* com.dlqudtjs.codingbattle.controller..*.*(..)) && !@annotation(com.dlqudtjs.codingbattle.configuration.log.NoLogging)")
+    public void allController() {
+    }
+
+    @Pointcut("execution(* com.dlqudtjs.codingbattle.service..*.*(..)) && !@annotation(com.dlqudtjs.codingbattle.configuration.log.NoLogging)")
+    public void allService() {
+    }
+
+    @Pointcut("execution(* com.dlqudtjs.codingbattle.repository..*.*(..)) && !@annotation(com.dlqudtjs.codingbattle.configuration.log.NoLogging)")
+    public void allRepository() {
+    }
+
+    @Around("allController() || allService() || allRepository()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Start - {}.{}", className, methodName);
+        Object result = joinPoint.proceed();
+        log.info("End - {}.{}", className, methodName);
+        return result;
+
+//        log.info("==============================execution method==============================");
+//        log.info("Target: {}", joinPoint.getTarget());
+//        log.info("Signature: {}", joinPoint.getSignature().getName());
+//
+//        Object result = joinPoint.proceed();
+//        return result;
+    }
+}
