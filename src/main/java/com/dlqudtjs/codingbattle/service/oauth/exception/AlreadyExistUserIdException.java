bea0d59bcc986db88d4858b116bbb9209d225fcc
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-28 16:36:45 +0900

    feat - AlreadyExistUserIdException 클래스 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/AlreadyExistUserIdException.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.service.oauth.exception;
+
+public class AlreadyExistUserIdException extends RuntimeException {
+        public AlreadyExistUserIdException(String message) {
+            super(message);
+        }
+}
