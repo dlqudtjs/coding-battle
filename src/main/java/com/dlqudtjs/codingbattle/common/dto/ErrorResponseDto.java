67ee1f69821b310f1871307ce3ed365646d39dd2
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-28 16:34:47 +0900

    feat - ErrorResponseDto 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/common/dto/ErrorResponseDto.java
@@ -0,0 +1,14 @@
+package com.dlqudtjs.codingbattle.common.dto;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ErrorResponseDto {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final String exception;
+    private final String message;
+}
