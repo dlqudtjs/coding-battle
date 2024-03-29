28ea290cbb8fc74f9bc1da264811a0fcdf6d3687
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-29 15:32:25 +0900

    feat - 닉네임 중복 에러 Exception class 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/AlreadyExistNicknameException.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.service.oauth.exception;
+
+public class AlreadyExistNicknameException extends RuntimeException {
+        public AlreadyExistNicknameException(String message) {
+            super(message);
+        }
+}
