5629fbfbb6fb3740689234c768c3d6dd3d960235
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-29 15:38:56 +0900

    feat - password check 에러 Exception class 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/PasswordCheckException.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.service.oauth.exception;
+
+public class PasswordCheckException extends RuntimeException {
+        public PasswordCheckException(String message) {
+            super(message);
+        }
+}
