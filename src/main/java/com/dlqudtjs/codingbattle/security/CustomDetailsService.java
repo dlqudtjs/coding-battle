f1e63e3fe710706a671cc8800fa7428ddb9e6fd2
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-31 18:28:32 +0900

    feat - UserDetailsService 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/security/CustomDetailsService.java
@@ -0,0 +1,22 @@
+package com.dlqudtjs.codingbattle.security;
+
+import com.dlqudtjs.codingbattle.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
+    }
+}
