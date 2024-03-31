1d4114a568088411dcc39dc3b2091ca8f77a0555
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-31 18:39:26 +0900

    feat - Authentication 반환 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/security/JwtTokenProvider.java
+++ src/main/java/com/dlqudtjs/codingbattle/security/JwtTokenProvider.java
@@ -1,31 +1,45 @@
 package com.dlqudtjs.codingbattle.security;
 
 import com.dlqudtjs.codingbattle.model.oauth.JwtToken;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class JwtTokenProvider {
 
-    private final Key key;
+    private Key key;
 
+    @Value("${jwt.secret-key}")
+    String secretKey;
     @Value("${jwt.access-token-validity-in-seconds}")
     private Long accessTokenValidityInMilliseconds;
     @Value("${jwt.refresh-token-validity-in-seconds}")
     private Long refreshTokenValidityInMilliseconds;
+    private final UserDetailsService userDetailsService;
 
-    public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
+    @PostConstruct
+    protected void init() {
         key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 
@@ -60,4 +74,43 @@ public class JwtTokenProvider {
                 .refreshToken(refreshToken)
                 .build();
     }
+
+    // Jwt 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
+    public Authentication getAuthentication(String accessToken) {
+        // Jwt 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private void validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+    }
+
+    public Claims parseClaims(String accessToken) {
+        validateToken(accessToken);
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(accessToken)
+                .getBody();
+    }
 }
+
+
