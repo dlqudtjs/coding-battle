13f6f544402954297814df9a12d4306a4e8aea59
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-31 19:13:10 +0900

    feat - Authorization Header 파싱 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/security/JwtTokenProvider.java
+++ src/main/java/com/dlqudtjs/codingbattle/security/JwtTokenProvider.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
@@ -73,6 +74,10 @@ public class JwtTokenProvider {
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
     }
 
     // Jwt 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
