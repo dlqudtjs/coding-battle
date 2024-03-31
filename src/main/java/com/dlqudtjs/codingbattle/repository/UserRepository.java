8213d65e35b8c1f16f7b8fd7f63eb7be24f64f99
dlqudtjs <dlqudtjs922@gmail.com>
2024-03-31 18:28:14 +0900

    feat - findByUserId 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/repository/UserRepository.java
+++ src/main/java/com/dlqudtjs/codingbattle/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.dlqudtjs.codingbattle.repository;
 
 import com.dlqudtjs.codingbattle.model.user.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUserId(String userId);
+
     boolean existsByUserId(String userId);
+
     boolean existsByNickname(String nickname);
 }
