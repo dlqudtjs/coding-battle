d92514393afcd0290e3643a3b0f5a90f8b9adcac
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-01 15:46:45 +0900

    feat - 로그인 성공 enum code 생성

--- src/main/java/com/dlqudtjs/codingbattle/service/oauth/SuccessCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/SuccessCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    SIGN_UP_SUCCESS(200, "회원가입 성공");
+    SIGN_UP_SUCCESS(200, "회원가입 성공"),
+    SIGN_IN_SUCCESS(200, "로그인 성공");
 
     private final int status;
     private final String message;
