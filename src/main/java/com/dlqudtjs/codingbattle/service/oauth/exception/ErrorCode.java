b7d4eabbc8227147de463ddfc998202b38d75ee2
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-01 15:05:37 +0900

    feat - PASSWORD_NOT_MATCH enum 추가

--- src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/ErrorCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     ALREADY_EXIST_USER_ID(409, "이미 존재하는 아이디입니다."),
     ALREADY_EXIST_NICKNAME(409, "이미 존재하는 닉네임입니다."),
     PASSWORD_CHECK(400, "비밀번호가 일치하지 않습니다."),
-    NOT_FOUND_USER_ID(400, "존재하지 않는 유저입니다.");
+    USER_ID_NOT_FOUNT(400, "존재하지 않는 유저입니다."),
+    PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다.");
 
     private final int status;
     private final String message;
