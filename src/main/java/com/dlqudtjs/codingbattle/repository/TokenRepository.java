22eb9495ff8c564ea142128518dc81f7a31d0dfa
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-01 15:57:58 +0900

    feat - tokenRepository 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/TokenRepository.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.repository;
+
+import org.antlr.v4.runtime.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+}
