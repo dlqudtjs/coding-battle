6644199780bc93fca50a58cdaca50122242b04e4
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-01 17:27:17 +0900

    feat - 로그인 controller 구현

--- src/main/java/com/dlqudtjs/codingbattle/controller/OAuthController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/OAuthController.java
@@ -1,10 +1,10 @@
 package com.dlqudtjs.codingbattle.controller;
 
 import com.dlqudtjs.codingbattle.common.dto.ResponseDto;
+import com.dlqudtjs.codingbattle.model.oauth.SignInRequestDto;
 import com.dlqudtjs.codingbattle.model.oauth.SignUpRequestDto;
 import com.dlqudtjs.codingbattle.service.oauth.OAuthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class OAuthController {
 
-        private final OAuthService oAuthService;
+    private final OAuthService oAuthService;
 
-        @PostMapping("/v1/oauth/sign-up")
-        public ResponseEntity<ResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
-                ResponseDto responseDto = oAuthService.singUp(signUpRequestDto);
+    @PostMapping("/v1/oauth/sign-up")
+    public ResponseEntity<ResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
+        ResponseDto responseDto = oAuthService.singUp(signUpRequestDto);
 
-                return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
-        }
+        return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
+    }
+
+    @PostMapping("/v1/oauth/sign-in")
+    public ResponseEntity<ResponseDto> signIn(@RequestBody SignInRequestDto signInRequestDto) {
+        ResponseDto responseDto = oAuthService.signIn(signInRequestDto);
+
+        return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
+    }
 }
