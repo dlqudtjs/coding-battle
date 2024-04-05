82cd8ba8ed5866322423ff0c09270b56303b3e2f
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-05 17:42:09 +0900

    feat - Jwt 관련 Exception Handler 구현

--- src/main/java/com/dlqudtjs/codingbattle/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dlqudtjs/codingbattle/common/exception/GlobalExceptionHandler.java
@@ -3,9 +3,17 @@ package com.dlqudtjs.codingbattle.common.exception;
 import com.dlqudtjs.codingbattle.common.dto.ErrorResponseDto;
 import com.dlqudtjs.codingbattle.service.oauth.exception.AlreadyExistNicknameException;
 import com.dlqudtjs.codingbattle.service.oauth.exception.AlreadyExistUserIdException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.CustomExpiredJwtException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.CustomMalformedJwtException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.CustomSignatureException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.CustomUnsupportedJwtException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.ErrorCode;
 import com.dlqudtjs.codingbattle.service.oauth.exception.PasswordCheckException;
 import com.dlqudtjs.codingbattle.service.oauth.exception.PasswordNotMatchException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.RefreshTokenNotFoundException;
+import com.dlqudtjs.codingbattle.service.oauth.exception.UnknownException;
 import com.dlqudtjs.codingbattle.service.oauth.exception.UserIdNotFoundException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -65,5 +73,48 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handlePasswordNotMatchException(PasswordNotMatchException e) {
         log.error("handlePasswordNotMatchException", e);
         return buildErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // 401 SignatureException
+    @ExceptionHandler(CustomSignatureException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleSignatureException(SignatureException e) {
+        log.error("handleSignatureException", e);
+        return buildErrorResponse(ErrorCode.SIGNATURE.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    // 401 MalformedJwtException
+    @ExceptionHandler(CustomMalformedJwtException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleMalformedJwtException() {
+        return buildErrorResponse(ErrorCode.MALFORMED_JWT.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    // 401 ExpiredJwtException
+    @ExceptionHandler(CustomExpiredJwtException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleExpiredJwtException() {
+        return buildErrorResponse(ErrorCode.EXPIRED_JWT.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    // 401 UnsupportedJwtException
+    @ExceptionHandler(CustomUnsupportedJwtException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleUnsupportedOperationException() {
+        return buildErrorResponse(ErrorCode.UNSUPPORTED_JWT.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    // 401 JwtUnknownException
+    @ExceptionHandler(UnknownException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleJwtUnknownException() {
+        return buildErrorResponse(ErrorCode.UNKNOWN.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    // 401 RefreshTokenNotFoundException
+    @ExceptionHandler(RefreshTokenNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleRefreshTokenNotFoundException(RefreshTokenNotFoundException e) {
+        return buildErrorResponse(ErrorCode.REFRESH_TOKEN_NOT_FOUND.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
