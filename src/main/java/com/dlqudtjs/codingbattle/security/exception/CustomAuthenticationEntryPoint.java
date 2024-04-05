f04b1d4e2ae75ee203b3f334a3bc96d58da51c89
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-05 17:44:06 +0900

    refactor - filter로 넘어온 exception 처리 기능 구현

--- src/main/java/com/dlqudtjs/codingbattle/security/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/dlqudtjs/codingbattle/security/exception/CustomAuthenticationEntryPoint.java
@@ -4,13 +4,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final HandlerExceptionResolver resolver;
+
+    public CustomAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        // JwtAuthenticationFilter에서 request에 담아서 보내준 예외를 처리
+        resolver.resolveException(request, response, null, (Exception) request.getAttribute("exception"));
     }
 }
