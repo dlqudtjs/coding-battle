aa4928fcb3150db146dc0a93088125128a650289
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-10 19:32:08 +0900

    feat - Stomp Config 기본 설정 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/WebSocketConfig.java
@@ -0,0 +1,50 @@
+package com.dlqudtjs.codingbattle.websocket.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket-endpoint")
+                .addInterceptors()
+                .setAllowedOriginPatterns("*");
+    }
+
+    // tcp handshake 시점에 jwt token을 검증하는 로직을 추가하려면 아래 메서드를 사용
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
