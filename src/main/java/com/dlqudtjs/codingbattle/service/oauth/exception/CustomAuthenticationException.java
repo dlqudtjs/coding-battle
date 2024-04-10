5029372ddce288bb330e7c2a0a12b14ac4b47448
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-10 19:32:36 +0900

    feat - 인증 관련 Exception 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/CustomAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.service.oauth.exception;
+
+public class CustomAuthenticationException extends RuntimeException {
+    public CustomAuthenticationException(String message) {
+        super(message);
+    }
+}
