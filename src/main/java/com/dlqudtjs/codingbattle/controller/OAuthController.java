f54cd0e5a34fb78a30bca55b4bde7f633a9d8757
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-11 23:30:33 +0900

    refactor - refresh-token 발급 http method get으로 변경

--- src/main/java/com/dlqudtjs/codingbattle/controller/OAuthController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/OAuthController.java
@@ -7,6 +7,7 @@ import com.dlqudtjs.codingbattle.service.oauth.OAuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class OAuthController {
         return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
     }
 
-    @PostMapping("/v1/oauth/refresh-token")
+    @GetMapping("/v1/oauth/refresh-token")
     public ResponseEntity<ResponseDto> refreshToken(HttpServletRequest request) {
         ResponseDto responseDto = oAuthService.refreshToken(request);
 
