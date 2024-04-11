5b7c5b4a26f595e57ff38df6157dee1ea338da4b
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-11 23:29:09 +0900

    feat - Stomp ChannelInterceptor 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/StompHandler.java
@@ -0,0 +1,39 @@
+package com.dlqudtjs.codingbattle.websocket.configuration;
+
+import com.dlqudtjs.codingbattle.common.constant.Header;
+import com.dlqudtjs.codingbattle.security.JwtTokenProvider;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@Slf4j
+public class StompHandler implements ChannelInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+        // 기본적인 socket session info logging
+        log.info("headerAccessor: {}", headerAccessor);
+
+        if (headerAccessor.getCommand() == StompCommand.CONNECT) {
+            String token = headerAccessor.getFirstNativeHeader(Header.AUTHORIZATION.getHeaderName());
+
+            jwtTokenProvider.validateToken(token);
+        }
+
+        return message;
+    }
+}
