8a950c6e7491ac9851f12f77887891331065237c
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-15 16:41:29 +0900

    remove - RefreshTokenNotFoundException 삭제

--- src/main/java/com/dlqudtjs/codingbattle/service/oauth/exception/RefreshTokenNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dlqudtjs.codingbattle.service.oauth.exception;
-
-public class RefreshTokenNotFoundException extends RuntimeException {
-    public RefreshTokenNotFoundException(String message) {
-        super(message);
-    }
-}
