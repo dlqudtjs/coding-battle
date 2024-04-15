8d6ddf3f027316151f02fb51f24cd0b19cb5070e
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-15 16:41:50 +0900

    feat - 아이디 중복 확인 성공 enum 생성

--- src/main/java/com/dlqudtjs/codingbattle/service/oauth/SuccessCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/oauth/SuccessCode.java
@@ -10,7 +10,7 @@ public enum SuccessCode {
     SIGN_UP_SUCCESS(200, "회원가입 성공"),
     SIGN_IN_SUCCESS(200, "로그인 성공"),
     REFRESH_TOKEN_SUCCESS(200, "토큰 재발급 성공"),
-    ;
+    CHECK_USER_ID_SUCCESS(200, "아이디 중복 확인 성공");
 
     private final int status;
     private final String message;
