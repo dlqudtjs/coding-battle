7e228e19d64ed66a7d1c56b673b4d260aa82fd77
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-19 02:53:00 +0900

    feat - 같은 유저가 같은 방에 들어가는 오류 enum 추가

--- src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     NOT_CONNECT_USER(400, "연결되지 않은 사용자입니다."),
     NOT_EXIST_ROOM(400, "존재하지 않는 방입니다."),
     FULL_ROOM(400, "방이 가득 찼습니다."),
-    NOT_EXIST_USER_IN_ROOM(400, "방에 존재하지 않는 사용자입니다.");
+    NOT_EXIST_USER_IN_ROOM(400, "방에 존재하지 않는 사용자입니다."),
+    SAME_USER_IN_ROOM(400, "같은 사용자가 방에 존재합니다.");
 
     private final int status;
     private final String message;
