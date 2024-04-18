59136109471bf64e000756b9033ddda83774b7ee
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 20:53:50 +0900

    feat - 방에 메시지 보내는 Dto 클래스 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/model/socket/SendToRoomMessageDto.java
@@ -0,0 +1,15 @@
+package com.dlqudtjs.codingbattle.model.socket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SendToRoomMessageDto {
+
+    private String roomId;
+    private String senderId;
+    private String message;
+}
