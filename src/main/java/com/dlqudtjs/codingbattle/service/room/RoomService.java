e6a18f36d6be6b101bbd5cdbc2da54ca551df036
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-19 01:23:03 +0900

    refactor - enterWaitRoom RequestDto 변경

--- src/main/java/com/dlqudtjs/codingbattle/service/room/RoomService.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/RoomService.java
@@ -2,10 +2,13 @@ package com.dlqudtjs.codingbattle.service.room;
 
 import com.dlqudtjs.codingbattle.common.dto.ResponseDto;
 import com.dlqudtjs.codingbattle.model.room.WaitRoomCreateRequestDto;
+import com.dlqudtjs.codingbattle.model.room.WaitRoomEnterRequestDto;
 
 public interface RoomService {
 
     ResponseDto createWaitRoom(WaitRoomCreateRequestDto requestDto, String token);
 
-    ResponseDto enterWaitRoom(WaitRoomCreateRequestDto requestDto);
+    ResponseDto enterWaitRoom(WaitRoomEnterRequestDto requestDto, String token);
+
+    ResponseDto leaveWaitRoom(String userId, Integer roomId);
 }
