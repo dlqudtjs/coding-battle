374c14f7aab6e0b5ad79698695a4e14158422134
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 20:48:25 +0900

    feat - CustomSocketException 추가

---
+++ src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/exception/CustomSocketException.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.websocket.configuration.exception;
+
+public class CustomSocketException extends RuntimeException {
+    public CustomSocketException(String message) {
+        super(message);
+    }
+}
