21c18a7fe1f380ab64d5e74a742d797bdd6b6ce2
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 23:40:55 +0900

    feat - connect, disconnect 시 세션 관리

--- src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/StompHandler.java
+++ src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/StompHandler.java
@@ -1,8 +1,8 @@
 package com.dlqudtjs.codingbattle.websocket.configuration;
 
 import com.dlqudtjs.codingbattle.common.constant.Header;
+import com.dlqudtjs.codingbattle.repository.socket.sessiontatus.SessionStatusRepository;
 import com.dlqudtjs.codingbattle.security.JwtTokenProvider;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Component;
 public class StompHandler implements ChannelInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final SessionStatusRepository sessionStatusRepository;
 
     @Override
     public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
@@ -33,21 +34,15 @@ public class StompHandler implements ChannelInterceptor {
         if (headerAccessor.getCommand() == StompCommand.CONNECT) {
             String token = headerAccessor.getFirstNativeHeader(Header.AUTHORIZATION.getHeaderName());
 
+            // token이 유효한지 확인
             jwtTokenProvider.validateToken(token);
+
             String userId = jwtTokenProvider.getUserName(token);
-            // todo 세션이 전체 구독하고 있는 토픽을 구독하는 로직 추가
 
-            // todo 중복 접속으로 끊어진 세션에게 알림을 보내기 위한 로직 추가
-//            String sessionId = WebsocketSessionHolder.getSessionId(userId);
-//            if (sessionId != null) {
-//                messagingTemplate.convertAndSend(sessionId, "이미 다른 기기에서 접속되어 있습니다.");
-//            }
-
-            try {
-                WebsocketSessionHolder.addSession(userId, headerAccessor.getSessionId());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            // userId의 세션 상태 추가
+            sessionStatusRepository.addSessionStatus(userId);
+            // userId와 sessionId를 매핑
+            WebsocketSessionHolder.addSession(userId, headerAccessor.getSessionId());
         }
 
         if (headerAccessor.getCommand() == StompCommand.SEND) {
@@ -56,6 +51,15 @@ public class StompHandler implements ChannelInterceptor {
             log.info("destination: {}", headerAccessor.getDestination());
             String str = new String((byte[]) message.getPayload(), StandardCharsets.UTF_8);
             log.info("message: {}", str);
+        }
+
+        if (headerAccessor.getCommand() == StompCommand.DISCONNECT) {
+            String userId = WebsocketSessionHolder.getUserIdFromSessionId(headerAccessor.getSessionId());
+
+            // userId의 세션 상태 삭제
+            sessionStatusRepository.removeSessionStatus(userId);
+            // userId와 sessionId 매핑 삭제
+            WebsocketSessionHolder.removeSessionFromUserId(userId);
         }
 
         return message;
