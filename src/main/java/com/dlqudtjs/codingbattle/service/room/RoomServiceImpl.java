ac488a525d74eb29a102c83aa43df2dbbe623342
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 23:53:24 +0900

    refactor - 방 생성 requestDto 유효성 검사 로직 분리

--- src/main/java/com/dlqudtjs/codingbattle/service/room/RoomServiceImpl.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/RoomServiceImpl.java
@@ -7,6 +7,7 @@ import com.dlqudtjs.codingbattle.repository.socket.room.RoomRepository;
 import com.dlqudtjs.codingbattle.security.JwtTokenProvider;
 import com.dlqudtjs.codingbattle.service.room.exception.CustomRoomException;
 import com.dlqudtjs.codingbattle.service.room.exception.ErrorCode;
+import com.dlqudtjs.codingbattle.service.session.SessionService;
 import com.dlqudtjs.codingbattle.websocket.configuration.WebsocketSessionHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,27 +17,24 @@ import org.springframework.stereotype.Service;
 public class RoomServiceImpl implements RoomService {
 
     private final RoomRepository roomRepository;
+    private final SessionService sessionService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public ResponseDto createWaitRoom(WaitRoomCreateRequestDto requestDto, String token) {
         String userId = jwtTokenProvider.getUserName(token);
 
-        // 요청한 유저가 방장이 아니면 예외 발생
-        if (!userId.equals(requestDto.getHostId())) {
-            throw new CustomRoomException(ErrorCode.INVALID_REQUEST.getMessage());
-        }
+        validateCreateWaitRoomRequest(requestDto, userId);
 
-        // 요청한 유저가 웹 소켓 세션에 존재하지 않으면
-        if (!WebsocketSessionHolder.existUser(requestDto.getHostId())) {
-            throw new CustomRoomException(ErrorCode.NOT_CONNECT_USER.getMessage());
-        }
-
-        // 이미 다른 방에 있다면 해당 방 나가고 새로운 방 생성
-
+        // 방장 설정
         WaitRoom room = requestDto.toEntity();
         room.addUser(requestDto.getHostId());
-        Integer createdRoomId = roomRepository.save(requestDto.toEntity());
+
+        // 방 생성
+        Integer createdRoomId = roomRepository.save(room);
+
+        // 유저의 세션 상태 변경
+        sessionService.enterRoom(userId, createdRoomId);
 
         return ResponseDto.builder()
                 .status(SuccessCode.CREATE_WAIT_ROOM_SUCCESS.getStatus())
@@ -45,9 +43,20 @@ public class RoomServiceImpl implements RoomService {
                 .build();
     }
 
-
     @Override
     public ResponseDto enterWaitRoom(WaitRoomCreateRequestDto requestDto) {
         return null;
+    }
+
+    private void validateCreateWaitRoomRequest(WaitRoomCreateRequestDto requestDto, String userId) {
+        // 요청한 유저가 방장이 아니면 예외 발생
+        if (!userId.equals(requestDto.getHostId())) {
+            throw new CustomRoomException(ErrorCode.INVALID_REQUEST.getMessage());
+        }
+
+        // 요청한 유저가 웹 소켓 세션에 존재하지 않으면
+        if (!WebsocketSessionHolder.existUser(userId)) {
+            throw new CustomRoomException(ErrorCode.NOT_CONNECT_USER.getMessage());
+        }
     }
 }
