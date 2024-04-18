d06be01d690c67a1c59708d8b7d5107ed5338c74
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-19 01:16:12 +0900

    rename - 방 퇴장 시 상태 변경 메서드명 변경

--- src/main/java/com/dlqudtjs/codingbattle/service/session/SessionServiceImpl.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/session/SessionServiceImpl.java
@@ -4,6 +4,10 @@ import com.dlqudtjs.codingbattle.repository.socket.sessiontatus.SessionStatusRep
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/*
+ * SessionServiceImpl 클래스는 Session의 상태를 관리하는 클래스임
+ * ex. 어디 방에 있는지, 방해 금지 상태인지 등
+ */
 @Service
 @RequiredArgsConstructor
 public class SessionServiceImpl implements SessionService {
@@ -17,6 +21,6 @@ public class SessionServiceImpl implements SessionService {
 
     @Override
     public void exitRoom(String userId) {
-        sessionStatusRepository.exitRoom(userId);
+        sessionStatusRepository.leaveRoom(userId);
     }
 }
