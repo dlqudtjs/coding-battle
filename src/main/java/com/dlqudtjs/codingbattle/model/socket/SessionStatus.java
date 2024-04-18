759f86087df1b51a15c6e928aa6a7f3545659bf4
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 23:48:43 +0900

    feat - Session의 상태를 저장하는 클래스 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/model/socket/SessionStatus.java
@@ -0,0 +1,24 @@
+package com.dlqudtjs.codingbattle.model.socket;
+
+import lombok.Getter;
+
+/*
+ * 세션 상태를 나타내는 클래스
+ */
+@Getter
+public class SessionStatus {
+
+    private Integer enterRoomId;
+    // todo : 추후 세션 상태 추가 (ex. 방해 금지)
+
+    public SessionStatus() {
+    }
+
+    public void enterRoom(Integer roomId) {
+        this.enterRoomId = roomId;
+    }
+
+    public void exitRoom() {
+        this.enterRoomId = null;
+    }
+}
