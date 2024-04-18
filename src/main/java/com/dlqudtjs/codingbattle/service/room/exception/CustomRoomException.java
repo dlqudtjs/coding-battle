1dbaa4a9fb4449f917f701d4fc3eada3a4ead8c3
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 20:47:04 +0900

    feat - CustomRoomException 추가

---
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/exception/CustomRoomException.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.service.room.exception;
+
+public class CustomRoomException extends RuntimeException {
+    public CustomRoomException(String message) {
+        super(message);
+    }
+}
