9bcaa193066c29178632977098b0db7ed3cf5e51
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 21:01:25 +0900

    feat - 대기방을 생성하는 Controller 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/controller/RoomController.java
@@ -0,0 +1,33 @@
+package com.dlqudtjs.codingbattle.controller;
+
+import com.dlqudtjs.codingbattle.common.dto.ResponseDto;
+import com.dlqudtjs.codingbattle.model.room.WaitRoomCreateRequestDto;
+import com.dlqudtjs.codingbattle.service.room.WaitRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final WaitRoomService waitRoomService;
+
+    @PostMapping("/v1/waitRoom")
+    public ResponseEntity<ResponseDto> createRoom(@RequestBody WaitRoomCreateRequestDto requestDto,
+                                                  @RequestHeader("Authorization") String token) {
+        ResponseDto responseDto = waitRoomService.createWaitRoom(requestDto, token);
+
+        return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
+    }
+
+    @PostMapping("/v1/waitRoom/enter")
+    public ResponseEntity<ResponseDto> enterRoom(@RequestBody WaitRoomCreateRequestDto requestDto) {
+        ResponseDto responseDto = waitRoomService.enterWaitRoom(requestDto);
+
+        return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
+    }
+}
