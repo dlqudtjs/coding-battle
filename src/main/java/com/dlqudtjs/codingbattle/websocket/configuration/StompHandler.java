95b704f6faa22b92a7631bda5d89013b16f2e152
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 20:43:48 +0900

    feat - 웹 소켓 연결 시 userId와 매칭하는 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/StompHandler.java
+++ src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/StompHandler.java
@@ -2,6 +2,8 @@ package com.dlqudtjs.codingbattle.websocket.configuration;
 
 import com.dlqudtjs.codingbattle.common.constant.Header;
 import com.dlqudtjs.codingbattle.security.JwtTokenProvider;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,9 +34,28 @@ public class StompHandler implements ChannelInterceptor {
             String token = headerAccessor.getFirstNativeHeader(Header.AUTHORIZATION.getHeaderName());
 
             jwtTokenProvider.validateToken(token);
+            String userId = jwtTokenProvider.getUserName(token);
+            // todo 세션이 전체 구독하고 있는 토픽을 구독하는 로직 추가
 
-            // 기본으로 /queue/errors로 에러 메시지를 보내기 때문에 해당 메시지를 받아서 처리하려면 아래 주석을 해제
-            headerAccessor.setDestination("/queue/errors");
+            // todo 중복 접속으로 끊어진 세션에게 알림을 보내기 위한 로직 추가
+//            String sessionId = WebsocketSessionHolder.getSessionId(userId);
+//            if (sessionId != null) {
+//                messagingTemplate.convertAndSend(sessionId, "이미 다른 기기에서 접속되어 있습니다.");
+//            }
+
+            try {
+                WebsocketSessionHolder.addSession(userId, headerAccessor.getSessionId());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (headerAccessor.getCommand() == StompCommand.SEND) {
+            // todo 방에 메시지를 보낼 때 해당 방에 존재하는지 확인하는 로직 추가
+            log.info("session id: {}", headerAccessor.getSessionId());
+            log.info("destination: {}", headerAccessor.getDestination());
+            String str = new String((byte[]) message.getPayload(), StandardCharsets.UTF_8);
+            log.info("message: {}", str);
         }
 
         return message;
