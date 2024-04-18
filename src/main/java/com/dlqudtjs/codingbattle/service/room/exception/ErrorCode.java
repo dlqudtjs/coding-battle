536267df761ad5fea7af9473e9109bef0ac1b946
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-19 01:58:12 +0900

    feat - 방이 꽉 찼을 때 에러 enum 추가

--- src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     INVALID_REQUEST(400, "올바르지 않은 요청입니다."),
     NOT_CONNECT_USER(400, "연결되지 않은 사용자입니다."),
     NOT_EXIST_ROOM(400, "존재하지 않는 방입니다."),
+    FULL_ROOM(400, "방이 가득 찼습니다."),
     ;
 
     private final int status;
