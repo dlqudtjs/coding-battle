786b6142498f40f7ae64b4b187d804f3864b1467
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-18 20:48:50 +0900

    feat - room ErrorCode enum 추가

---
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.dlqudtjs.codingbattle.service.room.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INVALID_REQUEST(400, "올바르지 않은 요청입니다."),
+    NOT_CONNECT_USER(400, "연결되지 않은 사용자입니다.");
+
+    private final int status;
+    private final String message;
+}
