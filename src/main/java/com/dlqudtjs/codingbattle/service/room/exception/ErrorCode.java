4bea1893ec18e378608941219e90cce15f777ffe
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-19 00:52:57 +0900

    feat - 방이 존재하지 않을 때 에러 enum 추가

--- src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INVALID_REQUEST(400, "올바르지 않은 요청입니다."),
-    NOT_CONNECT_USER(400, "연결되지 않은 사용자입니다.");
+    NOT_CONNECT_USER(400, "연결되지 않은 사용자입니다."),
+    NOT_EXIST_ROOM(400, "존재하지 않는 방입니다."),
+    ;
 
     private final int status;
     private final String message;
