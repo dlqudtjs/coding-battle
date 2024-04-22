783df9217d491a80a309000161f784cc2123720d
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-22 21:35:00 +0900

    feat - 게임 설정 enum 추가

---
+++ src/main/java/com/dlqudtjs/codingbattle/common/constant/GameSetting.java
@@ -0,0 +1,25 @@
+package com.dlqudtjs.codingbattle.common.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum GameSetting {
+    MIN_USER_COUNT(2),
+    MAX_USER_COUNT(4),
+    MIN_PROBLEM_LEVEL(1),
+    MAX_PROBLEM_LEVEL(10),
+    MIN_SUBMISSION_COUNT(1),
+    MAX_SUBMISSION_COUNT(10),
+    MIN_LIMIT_TIME(10),
+    MAX_LIMIT_TIME(120);
+
+    private final int value;
+
+    private GameSetting(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
