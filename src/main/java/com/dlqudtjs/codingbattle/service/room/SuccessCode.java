4003c51d2dbcf8eef2234c8d5667ff4c688aafe5
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-22 18:08:07 +0900

    feat - gameRoom 반환 eunm 구현

--- src/main/java/com/dlqudtjs/codingbattle/service/room/SuccessCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/room/SuccessCode.java
@@ -7,9 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    CREATE_WAIT_ROOM_SUCCESS(201, "대기방 생성 성공"),
-    JOIN_WAIT_ROOM_SUCCESS(200, "대기방 입장 성공"),
-    LEAVE_WAIT_ROOM_SUCCESS(200, "대기방 퇴장"),
+    CREATE_GAME_ROOM_SUCCESS(201, "대기방 생성 성공"),
+    JOIN_GAME_ROOM_SUCCESS(200, "대기방 입장 성공"),
+    LEAVE_GAME_ROOM_SUCCESS(200, "대기방 퇴장"),
+    GET_GAME_ROOM_LIST_SUCCESS(200, "대기방 목록 조회 성공"),
     ;
 
     private final int status;
