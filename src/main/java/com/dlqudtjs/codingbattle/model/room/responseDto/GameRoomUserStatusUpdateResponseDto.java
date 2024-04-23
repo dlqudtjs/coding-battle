33717bd337553449aede81218b5c33a397cf25bd
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-23 10:10:22 +0900

    rename - Response 패키지로 이동

--- src/main/java/com/dlqudtjs/codingbattle/model/room/responseDto/GameRoomUserStatusUpdateResponseDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/model/room/responseDto/GameRoomUserStatusUpdateResponseDto.java
@@ -1,5 +1,6 @@
-package com.dlqudtjs.codingbattle.model.room.requestDto;
+package com.dlqudtjs.codingbattle.model.room.responseDto;
 
+import com.dlqudtjs.codingbattle.model.room.requestDto.GameRoomUserStatusUpdateRequestDto;
 import lombok.Builder;
 import lombok.Getter;
 
