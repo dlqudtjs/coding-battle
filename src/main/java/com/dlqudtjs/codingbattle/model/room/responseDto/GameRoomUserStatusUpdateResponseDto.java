80fa2df6cc84e597dbb9a9995d6a97c56f202ee0
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-23 10:11:56 +0900

    rename - 방 유저 업데이트 responseDto 수정

--- src/main/java/com/dlqudtjs/codingbattle/model/room/responseDto/GameRoomUserStatusUpdateResponseDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/model/room/responseDto/GameRoomUserStatusUpdateResponseDto.java
@@ -1,11 +1,10 @@
 package com.dlqudtjs.codingbattle.model.room.responseDto;
 
-import com.dlqudtjs.codingbattle.model.room.requestDto.GameRoomUserStatusUpdateRequestDto;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class GameRoomUserStatusUpdateResponseDto {
-    private GameRoomUserStatusUpdateRequestDto userStatus;
+    private GameRoomUserStatusResponseDto userStatus;
 }
