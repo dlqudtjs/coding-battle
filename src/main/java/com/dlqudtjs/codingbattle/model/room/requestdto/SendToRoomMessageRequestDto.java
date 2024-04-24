3f3415d84514a92c03684592f4413630a9289538
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-24 17:21:28 +0900

    rename - messageRequestDto 명 변경

---
+++ src/main/java/com/dlqudtjs/codingbattle/model/room/requestdto/SendToRoomMessageRequestDto.java
@@ -0,0 +1,9 @@
+package com.dlqudtjs.codingbattle.model.room.requestdto;
+
+import lombok.Getter;
+
+@Getter
+public class SendToRoomMessageRequestDto {
+    private String senderId;
+    private String message;
+}
