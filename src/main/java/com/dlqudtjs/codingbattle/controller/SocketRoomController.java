1c57aa3b899b3bfa2d9e1e66c4c8af7ab818a587
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-29 16:42:07 +0900

    feat - RoomService 의 메시지 전송 기능으로 변환

--- src/main/java/com/dlqudtjs/codingbattle/controller/SocketRoomController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/SocketRoomController.java
@@ -5,6 +5,9 @@ import com.dlqudtjs.codingbattle.common.exception.Custom4XXException;
 import com.dlqudtjs.codingbattle.model.room.requestdto.GameRoomStatusUpdateRequestDto;
 import com.dlqudtjs.codingbattle.model.room.requestdto.GameRoomUserStatusUpdateRequestDto;
 import com.dlqudtjs.codingbattle.model.room.requestdto.SendToRoomMessageRequestDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.SendToRoomMessageResponseDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.messagewrapperdto.GameRoomStatusUpdateMessageResponseDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.messagewrapperdto.GameRoomUserStatusUpdateMessageResponseDto;
 import com.dlqudtjs.codingbattle.service.room.RoomService;
 import com.dlqudtjs.codingbattle.service.room.exception.CustomRoomException;
 import com.dlqudtjs.codingbattle.websocket.configuration.exception.CustomSocketException;
@@ -28,29 +31,34 @@ public class SocketRoomController {
 
     @MessageMapping("/default/room")
     public void sendToDefaultRoom(String message, SimpMessageHeaderAccessor headerAccessor) {
+        // json -> dto
         SendToRoomMessageRequestDto sendToRoomMessageRequestDto =
                 (SendToRoomMessageRequestDto) parseMessage(message, new SendToRoomMessageRequestDto());
 
         try {
-            messagingTemplate.convertAndSend("/topic/default/room",
-                    roomService.parseMessage(GameSetting.DEFAULT_ROOM_ID.getValue(), headerAccessor.getSessionId(),
-                            sendToRoomMessageRequestDto));
+            // SendToRoomMessageResponseDto로 parse 및 유효성 검사
+            SendToRoomMessageResponseDto responseDto = roomService.parseMessage(
+                    GameSetting.DEFAULT_ROOM_ID.getValue(), headerAccessor.getSessionId(), sendToRoomMessageRequestDto);
+
+            roomService.sendToRoomMessage(GameSetting.DEFAULT_ROOM_ID.getValue(), responseDto);
         } catch (Custom4XXException e) {
             throw new CustomSocketException(ErrorCode.JSON_PARSE_ERROR.getMessage());
         }
-
     }
 
     @MessageMapping("/room/message/{roomId}")
     public void sendToRoom(@DestinationVariable("roomId") Integer roomId, String message,
                            SimpMessageHeaderAccessor headerAccessor) {
+        // json -> dto
         SendToRoomMessageRequestDto sendToRoomMessageRequestDto =
                 (SendToRoomMessageRequestDto) parseMessage(message, new SendToRoomMessageRequestDto());
 
         try {
-            messagingTemplate.convertAndSend("/topic/room/" + roomId,
-                    roomService.parseMessage(roomId, headerAccessor.getSessionId(),
-                            sendToRoomMessageRequestDto));
+            // SendToRoomMessageResponseDto로 parse 및 유효성 검사
+            SendToRoomMessageResponseDto responseDto = roomService.parseMessage(
+                    roomId, headerAccessor.getSessionId(), sendToRoomMessageRequestDto);
+
+            roomService.sendToRoomMessage(roomId, responseDto);
         } catch (CustomRoomException e) {
             throw new CustomRoomException(e.getMessage());
         }
@@ -59,15 +67,17 @@ public class SocketRoomController {
     @MessageMapping("/room/{roomId}/update/room-status")
     public void updateRoom(@DestinationVariable("roomId") Integer roomId, String message,
                            SimpMessageHeaderAccessor headerAccessor) {
+        // json -> dto
         GameRoomStatusUpdateRequestDto gameRoomStatusUpdateRequestDto =
                 (GameRoomStatusUpdateRequestDto) parseMessage(message, new GameRoomStatusUpdateRequestDto());
 
         try {
             gameRoomStatusUpdateRequestDto.validate();
 
-            messagingTemplate.convertAndSend("/topic/room/" + roomId,
-                    roomService.updateGameRoomStatus(roomId, headerAccessor.getSessionId(),
-                            gameRoomStatusUpdateRequestDto));
+            GameRoomStatusUpdateMessageResponseDto responseDto = roomService.updateGameRoomStatus(
+                    roomId, headerAccessor.getSessionId(), gameRoomStatusUpdateRequestDto);
+
+            roomService.sendToRoomMessage(roomId, responseDto);
         } catch (Custom4XXException e) {
             throw new CustomSocketException(ErrorCode.JSON_PARSE_ERROR.getMessage());
         } catch (CustomRoomException e) {
@@ -78,15 +88,17 @@ public class SocketRoomController {
     @MessageMapping("/room/{roomId}/update/user-status")
     public void updateUserStatus(@DestinationVariable("roomId") Integer roomId, String message,
                                  SimpMessageHeaderAccessor headerAccessor) {
+        // json -> dto
         GameRoomUserStatusUpdateRequestDto gameRoomUserStatusUpdateRequestDto =
                 (GameRoomUserStatusUpdateRequestDto) parseMessage(message, new GameRoomUserStatusUpdateRequestDto());
 
         try {
             gameRoomUserStatusUpdateRequestDto.validate();
 
-            messagingTemplate.convertAndSend("/topic/room/" + roomId,
-                    roomService.updateGameRoomUserStatus(roomId, headerAccessor.getSessionId(),
-                            gameRoomUserStatusUpdateRequestDto));
+            GameRoomUserStatusUpdateMessageResponseDto responseDto = roomService.updateGameRoomUserStatus(
+                    roomId, headerAccessor.getSessionId(), gameRoomUserStatusUpdateRequestDto);
+
+            roomService.sendToRoomMessage(roomId, responseDto);
         } catch (Custom4XXException e) {
             throw new CustomSocketException(ErrorCode.JSON_PARSE_ERROR.getMessage());
         } catch (CustomRoomException e) {
@@ -99,6 +111,7 @@ public class SocketRoomController {
         messagingTemplate.convertAndSend("/topic/error", e.getMessage());
     }
 
+    // parse json -> requestDto
     private Object parseMessage(String message, Object requestDto) {
         try {
             return objectMapper.readValue(message, requestDto.getClass());
