82e5caef7d38e6043f312a422af21b72b2971749
dlqudtjs <dlqudtjs922@gmail.com>
2024-04-29 18:42:01 +0900

    feat - RoomController에서 socketRoomController를 이용하여 소켓 메시지 전송하는 로직 추가

--- src/main/java/com/dlqudtjs/codingbattle/controller/RoomController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/RoomController.java
@@ -1,8 +1,14 @@
 package com.dlqudtjs.codingbattle.controller;
 
+import com.dlqudtjs.codingbattle.common.constant.ProgrammingLanguage;
 import com.dlqudtjs.codingbattle.common.dto.ResponseDto;
 import com.dlqudtjs.codingbattle.model.room.requestdto.GameRoomCreateRequestDto;
 import com.dlqudtjs.codingbattle.model.room.requestdto.GameRoomEnterRequestDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.GameRoomInfoResponseDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.GameRoomLeaveUserStatusResponseDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.GameRoomUserStatusResponseDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.messagewrapperdto.GameRoomEnterUserStatusMessageResponseDto;
+import com.dlqudtjs.codingbattle.model.room.responsedto.messagewrapperdto.GameRoomLeaveUserStatusMessageResponseDto;
 import com.dlqudtjs.codingbattle.service.room.RoomService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +25,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomController {
 
     private final RoomService gameRoomService;
+    private final SocketRoomController socketRoomController;
 
     @PostMapping("/v1/gameRoom")
     public ResponseEntity<ResponseDto> createRoom(@Valid @RequestBody GameRoomCreateRequestDto requestDto,
                                                   @RequestHeader("Authorization") String token) {
         ResponseDto responseDto = gameRoomService.createGameRoom(requestDto, token);
+
+        // 방안에 사용자들에게 나간 유저의 정보를 전달
+        GameRoomInfoResponseDto gameRoomInfoResponseDto = (GameRoomInfoResponseDto) responseDto.getData();
+        if (gameRoomInfoResponseDto.getLeaveUserStatus() != null) {
+            socketRoomController.sendToRoom(
+                    gameRoomInfoResponseDto.getRoomStatus().getRoomId(),
+                    GameRoomLeaveUserStatusMessageResponseDto.builder()
+                            .leaveUserStatus(gameRoomInfoResponseDto.getLeaveUserStatus())
+                            .build()
+            );
+        }
 
         return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
     }
@@ -33,16 +51,46 @@ public class RoomController {
                                                  @RequestHeader("Authorization") String token) {
         ResponseDto responseDto = gameRoomService.enterGameRoom(requestDto, token);
 
+        // 방안에 사용자들에게 나간 유저의 정보를 전달
+        GameRoomInfoResponseDto gameRoomInfoResponseDto = (GameRoomInfoResponseDto) responseDto.getData();
+        if (gameRoomInfoResponseDto.getLeaveUserStatus() != null) {
+            socketRoomController.sendToRoom(
+                    gameRoomInfoResponseDto.getRoomStatus().getRoomId(),
+                    GameRoomLeaveUserStatusMessageResponseDto.builder()
+                            .leaveUserStatus(gameRoomInfoResponseDto.getLeaveUserStatus())
+                            .build()
+            );
+        }
+
+        // 방안에 사용자들에게 들어온 유저의 정보를 전달
+        GameRoomEnterUserStatusMessageResponseDto enterUserStatusResponseDto = GameRoomEnterUserStatusMessageResponseDto.builder()
+                .enterUserStatus(GameRoomUserStatusResponseDto.builder()
+                        .userId(requestDto.getUserId())
+                        .isReady(false)
+                        .language(ProgrammingLanguage.DEFAULT.getLanguageName())
+                        .build())
+                .build();
+        socketRoomController.sendToRoom(requestDto.getRoomId(), enterUserStatusResponseDto);
+
         return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
     }
 
     @PostMapping("/v1/gameRoom/leave/{roomId}")
     public ResponseEntity<ResponseDto> leaveRoom(@PathVariable("roomId") Integer roomId,
                                                  @RequestHeader("Authorization") String token) {
-
         ResponseDto responseDto = gameRoomService.leaveGameRoom(roomId, token);
 
-        return ResponseEntity.status(responseDto.getStatus()).body(responseDto);
+        GameRoomLeaveUserStatusResponseDto gameRoomLeaveUserStatusResponseDto =
+                (GameRoomLeaveUserStatusResponseDto) responseDto.getData();
+
+        socketRoomController.sendToRoom(
+                roomId,
+                GameRoomLeaveUserStatusMessageResponseDto.builder()
+                        .leaveUserStatus(gameRoomLeaveUserStatusResponseDto)
+                        .build()
+        );
+
+        return ResponseEntity.status(responseDto.getStatus()).body(null);
     }
 
     @GetMapping("/v1/gameRoomList")
