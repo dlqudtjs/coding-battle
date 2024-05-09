62ef9afd04d7930e59beaedfd8a8457b63fdb3c0
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-09 17:03:35 +0900

    refactor - SignUpRequestDto 클래스의 toEntity 메서드 삭제

--- src/main/java/com/dlqudtjs/codingbattle/model/oauth/SignUpRequestDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/model/oauth/SignUpRequestDto.java
@@ -1,7 +1,5 @@
 package com.dlqudtjs.codingbattle.model.oauth;
 
-import com.dlqudtjs.codingbattle.model.user.User;
-import com.dlqudtjs.codingbattle.common.constant.UserRoleType;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequestDto {
+    
     @NotBlank
     private String userId;
     @NotBlank
@@ -21,12 +20,4 @@ public class SignUpRequestDto {
     private String passwordCheck;
     @NotBlank
     private String language;
-
-    public User toEntity() {
-        return User.builder()
-                .userId(userId)
-                .password(password)
-                .role(UserRoleType.ROLE_USER)
-                .build();
-    }
 }
