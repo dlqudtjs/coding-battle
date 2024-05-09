d1d4e2802907f7cb33ddb3ce1a8a1a231a7dd03b
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-09 17:14:46 +0900

    feat - UserRole <-> db mapping

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/user/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package com.dlqudtjs.codingbattle.repository.user;
+
+import com.dlqudtjs.codingbattle.common.constant.UserRoleType;
+import com.dlqudtjs.codingbattle.model.user.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    UserRole findByName(UserRoleType name);
+}
