4c5813a6f3dcf3d7be61d9534ef842c3ea07941d
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-09 14:16:09 +0900

    rename - UserRole -> UserRoleType 클래스명 변경

---
+++ src/main/java/com/dlqudtjs/codingbattle/common/constant/UserRoleType.java
@@ -0,0 +1,6 @@
+package com.dlqudtjs.codingbattle.common.constant;
+
+public enum UserRoleType {
+
+    ROLE_USER, ROLE_ADMIN;
+}
--- src/main/java/com/dlqudtjs/codingbattle/model/user/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.dlqudtjs.codingbattle.model.user;
-
-public enum UserRole {
-
-    ROLE_USER, ROLE_ADMIN;
-}
