8388b46a4f41441b22a6b026a5f81557c8c52a17
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-14 00:34:06 +0900

    feat - ProblemTestCaseRepository Jpa 구현

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemTestCaseRepository.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.repository.problem;
+
+import com.dlqudtjs.codingbattle.entity.problem.ProblemTestCase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemTestCaseRepository extends JpaRepository<ProblemTestCase, Long> {
+}
