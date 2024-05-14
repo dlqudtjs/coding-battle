fd562a817db23014049e7dcdebf33b71a9e11bda
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-15 01:14:08 +0900

    feat - getProblemList 인수에 AlgorithmType 인수 추가

--- src/main/java/com/dlqudtjs/codingbattle/service/problem/ProblemService.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/problem/ProblemService.java
@@ -1,10 +1,11 @@
 package com.dlqudtjs.codingbattle.service.problem;
 
+import com.dlqudtjs.codingbattle.common.constant.AlgorithmType;
 import com.dlqudtjs.codingbattle.common.constant.ProblemLevelType;
 import com.dlqudtjs.codingbattle.entity.problem.Problem;
 import java.util.List;
 
 public interface ProblemService {
 
-    List<Problem> getProblemList(ProblemLevelType problemLevel, Integer count);
+    List<Problem> getProblemList(AlgorithmType algorithmType, ProblemLevelType problemLevel, Integer count);
 }
