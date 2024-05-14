79e82b8408de8093cc21d897cbe76a3080c08773
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-15 01:02:00 +0900

    feat - AlgorithmClassification JPA 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/problem/AlgorithmClassificationRepository.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.repository.problem;
+
+import com.dlqudtjs.codingbattle.entity.problem.AlgorithmClassification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlgorithmClassificationRepository extends JpaRepository<AlgorithmClassification, Long> {
+}
