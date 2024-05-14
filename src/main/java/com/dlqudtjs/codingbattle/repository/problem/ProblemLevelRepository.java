5569ece1940f1c024482905ee0a04dc5aa9ed8bc
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-15 01:13:20 +0900

    feat - ProblemLevelRepository의 findByName 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemLevelRepository.java
+++ src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemLevelRepository.java
@@ -4,4 +4,5 @@ import com.dlqudtjs.codingbattle.entity.problem.ProblemLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProblemLevelRepository extends JpaRepository<ProblemLevel, Long> {
+    ProblemLevel findByName(String name);
 }
