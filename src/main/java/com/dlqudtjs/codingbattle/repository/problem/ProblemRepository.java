88c2a7a8cf5853145aa1ecccbcd1d538ae6e3887
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-15 01:36:11 +0900

    feat - ProblemRepository에 문제를 랜덤으로 추출하는 프로시저 선언

--- src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemRepository.java
+++ src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemRepository.java
@@ -1,7 +1,12 @@
 package com.dlqudtjs.codingbattle.repository.problem;
 
 import com.dlqudtjs.codingbattle.entity.problem.Problem;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+
+    @Procedure(name = "getRandomProblems")
+    List<Problem> getRandomProblems(Long algorithmId, Long problemLevelId, Integer count);
 }
