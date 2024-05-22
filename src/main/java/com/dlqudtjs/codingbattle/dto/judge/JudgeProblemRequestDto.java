5fcfea904a173be0bd7640ebfc114ab7d5e9c543
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-22 21:19:49 +0900

    refactor - JudgeProblemRequestDto에 vaild 기능 제거

--- src/main/java/com/dlqudtjs/codingbattle/dto/judge/JudgeProblemRequestDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/judge/JudgeProblemRequestDto.java
@@ -1,19 +1,13 @@
 package com.dlqudtjs.codingbattle.dto.judge;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class JudgeProblemRequestDto {
 
-    @NotBlank
     private Long problemId;
-    @NotBlank
     private Long roomId;
-    @NotBlank
     private String userId;
-    @NotBlank
     private String language;
-    @NotBlank
     private String code;
 }
