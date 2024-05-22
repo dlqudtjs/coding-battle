046dfcd0e09f58695eeeb1c46a176141ad70ec6e
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-22 21:17:04 +0900

    feat - 문제 채점 결과 반환 controller permit 설정

--- src/main/java/com/dlqudtjs/codingbattle/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/dlqudtjs/codingbattle/configuration/security/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig {
                                 .requestMatchers("/v1/oauth/sign-in").permitAll()
                                 .requestMatchers("/v1/oauth/check-userId/**").permitAll()
                                 .requestMatchers("/socket-endpoint").permitAll()
+                                .requestMatchers("/v1/judge/results").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling((exceptionHandling) ->
