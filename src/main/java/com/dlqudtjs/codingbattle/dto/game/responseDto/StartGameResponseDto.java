50e0be182672993579f5554d90030d0316cb4b66
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-22 21:31:42 +0900

    feat - StartGameResponseDto problemList 타입 수정

--- src/main/java/com/dlqudtjs/codingbattle/dto/game/responseDto/StartGameResponseDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/game/responseDto/StartGameResponseDto.java
@@ -1,6 +1,5 @@
 package com.dlqudtjs.codingbattle.dto.game.responseDto;
 
-import com.dlqudtjs.codingbattle.entity.problem.Problem;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,5 +7,5 @@ import lombok.Getter;
 @Getter
 @Builder
 public class StartGameResponseDto {
-    private List<Problem> problemList;
+    private List<ProblemResponseDto> problemList;
 }
