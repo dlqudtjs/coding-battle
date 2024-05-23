ba68bed12cf2d0783f9e655e1e5d68d55ea01017
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-23 19:12:51 +0900

    refactor - 채점 결과를 rapper 클래스에 담아서 반환하는 로직 추가

--- src/main/java/com/dlqudtjs/codingbattle/controller/JudgeController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/JudgeController.java
@@ -2,10 +2,12 @@ package com.dlqudtjs.codingbattle.controller;
 
 import com.dlqudtjs.codingbattle.common.dto.ResponseDto;
 import com.dlqudtjs.codingbattle.dto.game.requestDto.JudgeResultRequestDto;
+import com.dlqudtjs.codingbattle.dto.game.responseDto.messagewrapperdto.JudgeResultMessageResponseDto;
 import com.dlqudtjs.codingbattle.dto.judge.JudgeProblemRequestDto;
 import com.dlqudtjs.codingbattle.service.judge.JudgeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
+@Log4j2
 public class JudgeController {
 
     @Value("${docker.secret.key}")
@@ -31,15 +34,22 @@ public class JudgeController {
 
     @PostMapping("/v1/judge/results")
     public ResponseEntity<ResponseDto> judgeResults(@RequestBody JudgeResultRequestDto JudgeResultRequestDto) {
+
         // secret key 검증
         if (!JudgeResultRequestDto.getSecretKey().equals(secretKey)) {
-            System.out.println("Invalid secret key");
+            log.error("JudgeResults - secret key is not matched");
             return ResponseEntity.badRequest().build();
         }
 
+        JudgeResultMessageResponseDto responseDto = JudgeResultMessageResponseDto.builder()
+                .judgeResult(JudgeResultRequestDto.toJudgeResultResponseDto())
+                .build();
+
+        log.info("JudgeUserResults : " + responseDto.getJudgeResult() +
+                ", JudgeResult : " + JudgeResultRequestDto.getResult());
+
         // 결과 전송
-        messagingTemplate.convertAndSend("/topic/room/" + JudgeResultRequestDto.getRoomId(),
-                JudgeResultRequestDto.toJudgeResultResponseDto());
+        messagingTemplate.convertAndSend("/topic/room/" + JudgeResultRequestDto.getRoomId(), responseDto);
 
         // 결과 저장 (마지막 테스트까지 통과, Fail, Error)
 
