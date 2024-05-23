fbe8ac720d2b89c3a68dd945082115f5162ba288
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-23 20:23:06 +0900

    remove - testcase repository 삭제

--- src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemTestCaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dlqudtjs.codingbattle.repository.problem;
-
-import com.dlqudtjs.codingbattle.entity.problem.ProblemTestCase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemTestCaseRepository extends JpaRepository<ProblemTestCase, Long> {
-}
