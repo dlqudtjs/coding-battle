e0de8214b19d99a7f8559aaa38fe34c70f226ffe
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-23 23:11:21 +0900

    refactor - MatchHistory 테이블의 problem 속성 삭제에 따른 Entity 클래스 수정

--- src/main/java/com/dlqudtjs/codingbattle/entity/game/MatchHistory.java
+++ src/main/java/com/dlqudtjs/codingbattle/entity/game/MatchHistory.java
@@ -1,13 +1,10 @@
 package com.dlqudtjs.codingbattle.entity.game;
 
-import com.dlqudtjs.codingbattle.entity.problem.Problem;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
@@ -24,10 +21,6 @@ public class MatchHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "problem_id", nullable = false)
-    private Problem problem;
 
     @Column(name = "start_time", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp startTime;
