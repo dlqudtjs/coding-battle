3442096f54cd7968cbfa049f2c241e93cee8a7ab
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-24 20:27:39 +0900

    feat - 게임 시작시 현재 시간과 매치 기록을 저장하는 Service 로직 구현

--- src/main/java/com/dlqudtjs/codingbattle/service/game/GameServiceImpl.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/game/GameServiceImpl.java
@@ -6,8 +6,10 @@ import com.dlqudtjs.codingbattle.dto.game.responseDto.ProblemInfoResponseDto;
 import com.dlqudtjs.codingbattle.entity.game.GameSession;
 import com.dlqudtjs.codingbattle.entity.problem.ProblemInfo;
 import com.dlqudtjs.codingbattle.entity.room.GameRoom;
+import com.dlqudtjs.codingbattle.service.match.MatchService;
 import com.dlqudtjs.codingbattle.service.problem.ProblemService;
 import com.dlqudtjs.codingbattle.service.room.RoomService;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,6 +22,7 @@ public class GameServiceImpl implements GameService {
     static Map<Long, GameSession> gameSessionMap = new ConcurrentHashMap<>();
     private final RoomService roomService;
     private final ProblemService problemService;
+    private final MatchService matchService;
 
     @Override
     public List<ProblemInfoResponseDto> startGame(GameStartRequestDto requestDto) {
@@ -28,12 +31,17 @@ public class GameServiceImpl implements GameService {
 
         // 난이도에 따른 문제 리스트 가져오기
         ProblemLevelType problemLevel = gameRoom.getProblemLevel();
+        List<ProblemInfo> problemInfoList = problemService.getProblemInfoList(null, problemLevel, 1);
         GameSession gameSession = GameSession.builder()
                 .gameRoom(gameRoom)
-                .problemInfoList(problemService.getProblemInfoList(null, problemLevel, 1))
+                .problemInfoList(problemInfoList)
+                .startTime(new Timestamp(System.currentTimeMillis()))
                 .build();
 
         gameSessionMap.put(requestDto.getRoomId(), gameSession);
+
+        // 매치 기록 저장
+        matchService.startMatch(gameSession);
 
         return gameSession.getProblemInfo();
     }
