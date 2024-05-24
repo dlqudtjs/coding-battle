bf6b8dc9cc35c65bd307efaeada2e786f931b04e
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-24 19:59:41 +0900

    feat - MatchingResultClassificationRepository Jpa 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/game/MatchingResultClassificationRepository.java
@@ -0,0 +1,9 @@
+package com.dlqudtjs.codingbattle.repository.game;
+
+import com.dlqudtjs.codingbattle.entity.game.MatchingResultClassification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchingResultClassificationRepository extends
+        JpaRepository<MatchingResultClassification, Long> {
+
+}
