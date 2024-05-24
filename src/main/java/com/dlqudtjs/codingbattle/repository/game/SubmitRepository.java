76904456975949d0eb1ca1ee15250e8daeaf1b41
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-24 21:03:00 +0900

    feat - SubmitRepository Jpa 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/game/SubmitRepository.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.repository.game;
+
+import com.dlqudtjs.codingbattle.entity.submit.Submit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubmitRepository extends JpaRepository<Submit, Long> {
+}
