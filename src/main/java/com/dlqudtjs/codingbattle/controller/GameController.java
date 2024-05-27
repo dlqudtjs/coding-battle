b01fc86368d851e368e44e61f58eebc1ccace699
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-28 00:19:56 +0900

    feat : 게임 종료 컨트롤러 생성

--- src/main/java/com/dlqudtjs/codingbattle/controller/GameController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/GameController.java
@@ -1,10 +1,14 @@
 package com.dlqudtjs.codingbattle.controller;
 
 import com.dlqudtjs.codingbattle.common.dto.ResponseDto;
+import com.dlqudtjs.codingbattle.dto.game.requestDto.GameEndRequestDto;
 import com.dlqudtjs.codingbattle.dto.game.requestDto.GameStartRequestDto;
+import com.dlqudtjs.codingbattle.dto.game.responseDto.GameEndResponseDto;
 import com.dlqudtjs.codingbattle.dto.game.responseDto.ProblemInfoResponseDto;
 import com.dlqudtjs.codingbattle.dto.game.responseDto.StartGameResponseDto;
+import com.dlqudtjs.codingbattle.dto.game.responseDto.messagewrapperdto.GameEndMessageResponseDto;
 import com.dlqudtjs.codingbattle.entity.game.GameSession;
+import com.dlqudtjs.codingbattle.entity.room.GameRoom;
 import com.dlqudtjs.codingbattle.service.game.GameService;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -38,5 +42,30 @@ public class GameController {
                 startGameResponseDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PostMapping("/v1/game/end")
+    public ResponseEntity<ResponseDto> endGame(@Valid @RequestBody GameEndRequestDto requestDto) {
+        gameService.endGame(requestDto);
+
+        GameEndResponseDto gameEndResponseDto = GameEndResponseDto.builder()
+                .userId("김동건")
+                .code("public class Main { public static void main(String[] args) { System.out.println(\"Hello, World!\"); } }")
+                .build();
+
+        ResponseDto responseDto = ResponseDto.builder()
+                .status(200)
+                .data(gameEndResponseDto)
+                .build();
+
+        // 방에 게임 문제 전송
+        messagingTemplate.convertAndSend("/topic/room/" + requestDto.getRoomId(),
+                GameEndMessageResponseDto.builder()
+                        .gameEnd(gameEndResponseDto)
+                        .build());
+
+        GameRoom gameRoom = gameService.initGameRoom(requestDto.getRoomId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 }
