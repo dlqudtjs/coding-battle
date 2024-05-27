16297261f88984f64c60542dca23f50bb28dbfc6
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-27 18:35:49 +0900

    refactor : JudgeProblemRequestDto 의 matchId 삭제

--- src/main/java/com/dlqudtjs/codingbattle/dto/game/responseDto/messagewrapperdto/JudgeResultMessageResponseDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/game/responseDto/messagewrapperdto/JudgeResultMessageResponseDto.java
@@ -1,11 +1,11 @@
 package com.dlqudtjs.codingbattle.dto.game.responseDto.messagewrapperdto;
 
-import com.dlqudtjs.codingbattle.dto.game.responseDto.JudgeResultResponseDto;
+import com.dlqudtjs.codingbattle.dto.game.responseDto.ParsedJudgeResultResponseDto;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class JudgeResultMessageResponseDto {
-    private JudgeResultResponseDto judgeResult;
+    private ParsedJudgeResultResponseDto judgeResult;
 }
--- src/main/java/com/dlqudtjs/codingbattle/dto/judge/JudgeProblemRequestDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/judge/JudgeProblemRequestDto.java
@@ -7,7 +7,6 @@ public class JudgeProblemRequestDto {
 
     private Long problemId;
     private Long roomId;
-    private Long matchId;
     private String userId;
     private String language;
     private String code;
