a3343f0f3895336c552bd290cab36be13d4502af
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-27 18:38:48 +0900

    feat : SubmitResultCodeRepository Jpa 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/repository/game/SubmitResultCodeRepository.java
@@ -0,0 +1,7 @@
+package com.dlqudtjs.codingbattle.repository.game;
+
+import com.dlqudtjs.codingbattle.entity.submit.SubmitResultCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubmitResultCodeRepository extends JpaRepository<SubmitResultCode, Long> {
+}
