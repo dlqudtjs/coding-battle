a9f93c8d62628c67c43286a82734c829e6ecb4b8
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-27 20:39:46 +0900

    refactor : JudgeResultCode enum에 테이블 id 매핑

--- src/main/java/com/dlqudtjs/codingbattle/common/constant/JudgeResultCode.java
+++ src/main/java/com/dlqudtjs/codingbattle/common/constant/JudgeResultCode.java
@@ -1,5 +1,15 @@
 package com.dlqudtjs.codingbattle.common.constant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum JudgeResultCode {
-    PASS, FAIL, ERROR
+    PENDING(0),
+    PASS(1),
+    FAIL(2),
+    ERROR(3);
+
+    private final int code;
 }
