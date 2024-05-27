7ba0039db4fba1a8a7c651e6b9083ac747829741
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-27 18:05:22 +0900

    feat : matchId를 GameSession에 기록하는 로직 추가

--- src/main/java/com/dlqudtjs/codingbattle/service/game/GameServiceImpl.java
+++ src/main/java/com/dlqudtjs/codingbattle/service/game/GameServiceImpl.java
@@ -1,9 +1,12 @@
 package com.dlqudtjs.codingbattle.service.game;
 
+import static com.dlqudtjs.codingbattle.common.exception.CommonErrorCode.INVALID_INPUT_VALUE;
+
 import com.dlqudtjs.codingbattle.common.constant.ProblemLevelType;
+import com.dlqudtjs.codingbattle.common.exception.Custom4XXException;
 import com.dlqudtjs.codingbattle.dto.game.requestDto.GameStartRequestDto;
-import com.dlqudtjs.codingbattle.dto.game.responseDto.ProblemInfoResponseDto;
 import com.dlqudtjs.codingbattle.entity.game.GameSession;
+import com.dlqudtjs.codingbattle.entity.game.MatchHistory;
 import com.dlqudtjs.codingbattle.entity.problem.ProblemInfo;
 import com.dlqudtjs.codingbattle.entity.room.GameRoom;
 import com.dlqudtjs.codingbattle.service.match.MatchService;
@@ -25,13 +28,14 @@ public class GameServiceImpl implements GameService {
     private final MatchService matchService;
 
     @Override
-    public List<ProblemInfoResponseDto> startGame(GameStartRequestDto requestDto) {
+    public GameSession startGame(GameStartRequestDto requestDto) {
         // 게임 시작
         GameRoom gameRoom = roomService.startGame(requestDto.getRoomId());
 
         // 난이도에 따른 문제 리스트 가져오기
         ProblemLevelType problemLevel = gameRoom.getProblemLevel();
         List<ProblemInfo> problemInfoList = problemService.getProblemInfoList(null, problemLevel, 1);
+
         GameSession gameSession = GameSession.builder()
                 .gameRoom(gameRoom)
                 .problemInfoList(problemInfoList)
@@ -41,9 +45,10 @@ public class GameServiceImpl implements GameService {
         gameSessionMap.put(requestDto.getRoomId(), gameSession);
 
         // 매치 기록 저장
-        matchService.startMatch(gameSession);
+        MatchHistory matchHistory = matchService.startMatch(gameSession);
+        gameSession.setMatchId(matchHistory.getId());
 
-        return gameSession.getProblemInfo();
+        return gameSession;
     }
 
     @Override
