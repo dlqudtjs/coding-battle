54114cfc8f940e0451c52f3aca319a57098ce2a9
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-28 14:01:32 +0900

    feat : 유저의 게임 상태를 저장하는 GameUserStatus 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/entity/game/GameUserStatus.java
@@ -0,0 +1,20 @@
+package com.dlqudtjs.codingbattle.entity.game;
+
+import com.dlqudtjs.codingbattle.entity.user.User;
+import lombok.Builder;
+
+@Builder
+public class GameUserStatus {
+    private User user;
+    private Boolean isSubmitDone;
+
+
+    public Boolean getIsSubmitDone() {
+        return isSubmitDone;
+    }
+
+    public Boolean toggleSubmitDone() {
+        isSubmitDone = !isSubmitDone;
+        return isSubmitDone;
+    }
+}
