f64a0da2d8526182eb6aecb21cbe3925251b6320
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-28 15:29:00 +0900

    feat - Winner 클래스에 MatchingResultType 변수 추가

--- src/main/java/com/dlqudtjs/codingbattle/entity/game/Winner.java
+++ src/main/java/com/dlqudtjs/codingbattle/entity/game/Winner.java
@@ -1,5 +1,6 @@
 package com.dlqudtjs.codingbattle.entity.game;
 
+import com.dlqudtjs.codingbattle.common.constant.MatchingResultType;
 import com.dlqudtjs.codingbattle.entity.user.User;
 import lombok.experimental.Delegate;
 
@@ -7,14 +8,20 @@ public class Winner extends User {
 
     @Delegate
     private final User user;
+    private final MatchingResultType matchingResultType;
     private final String code;
 
-    public Winner(User user, String code) {
+    public Winner(User user, MatchingResultType matchingResultType, String code) {
         this.user = user;
+        this.matchingResultType = matchingResultType;
         this.code = code;
     }
 
     public String getCode() {
         return code;
+    }
+
+    public MatchingResultType getMatchingResultType() {
+        return matchingResultType;
     }
 }
