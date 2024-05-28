9c7fb29304c5ce261d157cfe0725a1d8cbc7a3d7
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-28 13:56:10 +0900

    feat : Winner 클래스의 getCode 메서드 구현

--- src/main/java/com/dlqudtjs/codingbattle/entity/game/Winner.java
+++ src/main/java/com/dlqudtjs/codingbattle/entity/game/Winner.java
@@ -6,11 +6,15 @@ import lombok.experimental.Delegate;
 public class Winner extends User {
 
     @Delegate
-    private User user;
-    private String code;
+    private final User user;
+    private final String code;
 
     public Winner(User user, String code) {
         this.user = user;
         this.code = code;
+    }
+
+    public String getCode() {
+        return code;
     }
 }
