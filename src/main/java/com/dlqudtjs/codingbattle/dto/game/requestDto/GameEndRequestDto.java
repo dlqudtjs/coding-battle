da1ea34f8a415b518c246c0a44771b5c8137674c
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-28 18:33:58 +0900

    remove - 사용하지 않는 Dto 클래스 삭제

--- src/main/java/com/dlqudtjs/codingbattle/dto/game/requestDto/GameEndRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.dlqudtjs.codingbattle.dto.game.requestDto;
-
-import lombok.Getter;
-
-@Getter
-public class GameEndRequestDto {
-
-    private Long roomId;
-}
--- src/main/java/com/dlqudtjs/codingbattle/dto/game/requestDto/GameStartRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.dlqudtjs.codingbattle.dto.game.requestDto;
-
-import lombok.Getter;
-
-@Getter
-public class GameStartRequestDto {
-
-    private Long roomId;
-}
