9ebd540b51c0acb4cfd3a4da230d11eb34cc8097
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-31 15:28:37 +0900

    feat - WebSocketEventListener 구독 관련 listener 생성

--- src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/WebSocketEventListener.java
+++ src/main/java/com/dlqudtjs/codingbattle/websocket/configuration/WebSocketEventListener.java
@@ -1,20 +1,20 @@
 package com.dlqudtjs.codingbattle.websocket.configuration;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
 @Component
+@Log4j2
 public class WebSocketEventListener {
 
     @EventListener
     public void handleWebSocketSubscribeListener(SessionSubscribeEvent event) {
-        System.out.println("Received a new web socket connection");
     }
 
     @EventListener
     public void handleWebSocketUnsubscribeListener(SessionUnsubscribeEvent event) {
-        System.out.println("Received a web socket disconnect");
     }
 }
