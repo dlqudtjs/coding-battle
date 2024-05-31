53cb0ff3714cfa0f6e1160dc9f82ebd1efd421b0
dlqudtjs <dlqudtjs922@gmail.com>
2024-05-31 16:05:11 +0900

    refactor - SocketRoomController SendTo 어노테이션 활용 리팩토링

--- src/main/java/com/dlqudtjs/codingbattle/controller/SocketRoomController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/SocketRoomController.java
@@ -1,21 +1,19 @@
 package com.dlqudtjs.codingbattle.controller;
 
-import static com.dlqudtjs.codingbattle.common.constant.code.SocketConfigCode.JSON_PARSE_ERROR;
-
-import com.dlqudtjs.codingbattle.common.exception.Custom4XXException;
 import com.dlqudtjs.codingbattle.common.exception.CustomSocketException;
-import com.dlqudtjs.codingbattle.dto.room.requestdto.messagewrapperdto.RoomStatusUpdateMessageRequestDto;
 import com.dlqudtjs.codingbattle.dto.room.requestdto.RoomUserStatusUpdateRequestDto;
 import com.dlqudtjs.codingbattle.dto.room.requestdto.SendToRoomMessageRequestDto;
+import com.dlqudtjs.codingbattle.dto.room.requestdto.messagewrapperdto.RoomStatusUpdateMessageRequestDto;
 import com.dlqudtjs.codingbattle.dto.room.responsedto.SendToRoomMessageResponseDto;
 import com.dlqudtjs.codingbattle.dto.room.responsedto.messagewrapperdto.RoomStatusUpdateMessageResponseDto;
 import com.dlqudtjs.codingbattle.dto.room.responsedto.messagewrapperdto.RoomUserStatusUpdateMessageResponseDto;
 import com.dlqudtjs.codingbattle.service.room.RoomService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -26,25 +24,16 @@ public class SocketRoomController {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final RoomService roomService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @MessageMapping("/room/message/{roomId}")
-    public void sendToRoom(@DestinationVariable("roomId") Long roomId, String message,
-                           SimpMessageHeaderAccessor headerAccessor) {
-        // json -> dto
-        SendToRoomMessageRequestDto sendToRoomMessageRequestDto =
-                (SendToRoomMessageRequestDto) parseMessage(message, new SendToRoomMessageRequestDto());
+    @SendTo("/topic/room/{roomId}")
+    public SendToRoomMessageResponseDto sendToRoom(
+            @DestinationVariable("roomId") Long roomId,
+            @Payload SendToRoomMessageRequestDto sendToRoomMessageRequestDto,
+            SimpMessageHeaderAccessor headerAccessor) {
+        sendToRoomMessageRequestDto.validate();
 
-        try {
-            // SendToRoomMessageResponseDto로 parse 및 유효성 검사
-            SendToRoomMessageResponseDto responseDto = roomService.parseMessage(
-                    roomId, headerAccessor.getSessionId(), sendToRoomMessageRequestDto);
-
-            // 방에 메시지 전송
-            messagingTemplate.convertAndSend("/topic/room/" + roomId, responseDto);
-        } catch (Custom4XXException e) {
-            throw new Custom4XXException(e.getMessage(), e.getStatus());
-        }
+        return roomService.parseMessage(roomId, headerAccessor.getSessionId(), sendToRoomMessageRequestDto);
     }
 
     public void sendToRoom(Long roomId, Object responseDto) {
@@ -52,56 +41,29 @@ public class SocketRoomController {
     }
 
     @MessageMapping("/room/{roomId}/update/room-status")
-    public void updateRoom(@DestinationVariable("roomId") Long roomId, String message,
-                           SimpMessageHeaderAccessor headerAccessor) {
-        // json -> dto
-        RoomStatusUpdateMessageRequestDto roomStatusUpdateMessageRequestDto =
-                (RoomStatusUpdateMessageRequestDto)
-                        parseMessage(message, new RoomStatusUpdateMessageRequestDto());
-
+    @SendTo("/topic/room/{roomId}")
+    public RoomStatusUpdateMessageResponseDto updateRoom(
+            @DestinationVariable("roomId") Long roomId,
+            @Payload RoomStatusUpdateMessageRequestDto roomStatusUpdateMessageRequestDto,
+            SimpMessageHeaderAccessor headerAccessor) {
         roomStatusUpdateMessageRequestDto.validate();
-        try {
-            RoomStatusUpdateMessageResponseDto responseDto = roomService.updateRoomStatus(
-                    roomId, headerAccessor.getSessionId(), roomStatusUpdateMessageRequestDto);
 
-            // 방에 메시지 전송
-            messagingTemplate.convertAndSend("/topic/room/" + roomId, responseDto);
-        } catch (Custom4XXException e) {
-            throw new Custom4XXException(e.getMessage(), e.getStatus());
-        }
+        return roomService.updateRoomStatus(roomId, headerAccessor.getSessionId(), roomStatusUpdateMessageRequestDto);
     }
 
     @MessageMapping("/room/{roomId}/update/user-status")
-    public void updateUserStatus(@DestinationVariable("roomId") Long roomId, String message,
-                                 SimpMessageHeaderAccessor headerAccessor) {
-        // json -> dto
-        RoomUserStatusUpdateRequestDto roomUserStatusUpdateRequestDto =
-                (RoomUserStatusUpdateRequestDto) parseMessage(message, new RoomUserStatusUpdateRequestDto());
+    @SendTo("/topic/room/{roomId}")
+    public RoomUserStatusUpdateMessageResponseDto updateUserStatus(
+            @DestinationVariable("roomId") Long roomId,
+            @Payload RoomUserStatusUpdateRequestDto roomUserStatusUpdateRequestDto,
+            SimpMessageHeaderAccessor headerAccessor) {
+        roomUserStatusUpdateRequestDto.validate();
 
-        try {
-            roomUserStatusUpdateRequestDto.validate();
-
-            RoomUserStatusUpdateMessageResponseDto responseDto = roomService.updateRoomUserStatus(
-                    roomId, headerAccessor.getSessionId(), roomUserStatusUpdateRequestDto);
-
-            // 방에 메시지 전송
-            messagingTemplate.convertAndSend("/topic/room/" + roomId, responseDto);
-        } catch (Custom4XXException e) {
-            throw new Custom4XXException(e.getMessage(), e.getStatus());
-        }
+        return roomService.updateRoomUserStatus(roomId, headerAccessor.getSessionId(), roomUserStatusUpdateRequestDto);
     }
 
     @MessageExceptionHandler
     public void handleException(CustomSocketException e) {
         messagingTemplate.convertAndSend("/topic/error", e.getMessage());
-    }
-
-    // parse json -> requestDto
-    private Object parseMessage(String message, Object requestDto) {
-        try {
-            return objectMapper.readValue(message, requestDto.getClass());
-        } catch (Exception e) {
-            throw new CustomSocketException(JSON_PARSE_ERROR.getMessage());
-        }
     }
 }
--- src/main/java/com/dlqudtjs/codingbattle/dto/room/requestdto/SendToRoomMessageRequestDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/room/requestdto/SendToRoomMessageRequestDto.java
@@ -1,9 +1,21 @@
 package com.dlqudtjs.codingbattle.dto.room.requestdto;
 
+import static com.dlqudtjs.codingbattle.common.constant.code.CommonConfigCode.INVALID_INPUT_VALUE;
+
+import com.dlqudtjs.codingbattle.common.exception.Custom4XXException;
 import lombok.Getter;
 
 @Getter
 public class SendToRoomMessageRequestDto {
     private String senderId;
     private String message;
+
+    public void validate() {
+        if (senderId == null || senderId.isEmpty()) {
+            throw new Custom4XXException(INVALID_INPUT_VALUE.getMessage(), INVALID_INPUT_VALUE.getStatus());
+        }
+        if (message == null || message.isEmpty()) {
+            throw new Custom4XXException(INVALID_INPUT_VALUE.getMessage(), INVALID_INPUT_VALUE.getStatus());
+        }
+    }
 }
