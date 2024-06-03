0dca9ba87804a007cf55b9ae7054e227de34cf2c
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-03 23:50:46 +0900

    feat - 게임 시작 시 메시지를 전송하는 Dto 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/dto/game/responseDto/messagewrapperdto/GameStartMessageResponseDto.java
@@ -0,0 +1,10 @@
+package com.dlqudtjs.codingbattle.dto.game.responseDto.messagewrapperdto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GameStartMessageResponseDto {
+    private String startMessage;
+}
