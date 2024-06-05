9c1471e65befa181effcc69d8df1e8ec1a31ea84
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-05 21:23:05 +0900

    refactor - SendToRoomMessageRequestDto validate 메서드를 validation 라이브러리로 리팩토링

--- src/main/java/com/dlqudtjs/codingbattle/dto/room/requestdto/SendToRoomMessageRequestDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/room/requestdto/SendToRoomMessageRequestDto.java
@@ -1,21 +1,12 @@
 package com.dlqudtjs.codingbattle.dto.room.requestdto;
 
-import static com.dlqudtjs.codingbattle.common.constant.code.CommonConfigCode.INVALID_INPUT_VALUE;
-
-import com.dlqudtjs.codingbattle.common.exception.Custom4XXException;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SendToRoomMessageRequestDto {
+    @NotBlank
     private String senderId;
+    @NotBlank
     private String message;
-
-    public void validate() {
-        if (senderId == null || senderId.isEmpty()) {
-            throw new Custom4XXException(INVALID_INPUT_VALUE.getMessage(), INVALID_INPUT_VALUE.getStatus());
-        }
-        if (message == null || message.isEmpty()) {
-            throw new Custom4XXException(INVALID_INPUT_VALUE.getMessage(), INVALID_INPUT_VALUE.getStatus());
-        }
-    }
 }
