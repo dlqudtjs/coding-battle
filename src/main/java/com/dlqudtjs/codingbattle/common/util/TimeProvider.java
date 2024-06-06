72c7bdcd411f9e698f9c6d68ef2336d18937b1a6
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-06 21:26:19 +0900

    refactor - TimeProvider 클래스에 Date를 반환하는 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/common/util/TimeProvider.java
+++ src/main/java/com/dlqudtjs/codingbattle/common/util/TimeProvider.java
@@ -3,11 +3,16 @@ package com.dlqudtjs.codingbattle.common.util;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 public class TimeProvider {
 
     public static ZonedDateTime getZonedDateTime() {
         Instant now = Instant.now();
         return now.atZone(ZoneId.of("Asia/Seoul"));
+    }
+
+    public static Date getDate() {
+        return Date.from(getZonedDateTime().toInstant());
     }
 }
