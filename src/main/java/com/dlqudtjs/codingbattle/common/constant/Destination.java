ee54d4c35e13c4b1cf1723d36a2da0fff9455e04
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-06 15:32:37 +0900

    feat - Destination enum 클래스에 ERROR_BROADCAST 경로 추가

--- src/main/java/com/dlqudtjs/codingbattle/common/constant/Destination.java
+++ src/main/java/com/dlqudtjs/codingbattle/common/constant/Destination.java
@@ -7,9 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Destination {
     ROOM_BROADCAST("/topic/rooms/"),
-    ;
+    ERROR_BROADCAST("/topic/errors/");
 
     private final String value;
 
     public static final String ROOM_BROADCAST_VALUE = "/topic/rooms/";
+    public static final String ERROR_BROADCAST_VALUE = "/topic/errors/";
 }
