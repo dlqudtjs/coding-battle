49715a12310c708e6d4e7bf2521e7796c1a25896
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-14 02:41:02 +0900

    feat - Time 클래스에 경과 시간을 반환하는 기능 추가

--- src/main/java/com/dlqudtjs/codingbattle/common/util/Time.java
+++ src/main/java/com/dlqudtjs/codingbattle/common/util/Time.java
@@ -1,7 +1,9 @@
 package com.dlqudtjs.codingbattle.common.util;
 
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
@@ -19,6 +21,19 @@ public class Time {
 
     public static Timestamp getTimestamp() {
         return new Timestamp(getDate().getTime());
+    }
+
+    public static String getElapsedTime(Timestamp startTime, Timestamp endTime) {
+        LocalDateTime start = startTime.toLocalDateTime();
+        LocalDateTime end = endTime.toLocalDateTime();
+
+        Duration duration = Duration.between(start, end);
+
+        long hours = duration.toHours();
+        long minutes = duration.toMinutesPart();
+        long seconds = duration.toSecondsPart();
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
     public static ZonedDateTime convertTimestampToZonedDateTime(Timestamp timestamp) {
