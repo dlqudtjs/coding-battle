a304dc834348719d1cc1119c7769c7c4fdf9ce49
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-14 02:43:13 +0900

    feat - 매치에 참여한 유저 정보를 저장하는 클래스 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/entity/match/MatchRecodeUserStatus.java
@@ -0,0 +1,15 @@
+package com.dlqudtjs.codingbattle.entity.match;
+
+import com.dlqudtjs.codingbattle.common.constant.MatchingResultType;
+import com.dlqudtjs.codingbattle.common.constant.ProgrammingLanguage;
+import com.dlqudtjs.codingbattle.entity.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MatchRecodeUserStatus {
+
+    private User user;
+    private MatchingResultType result;
+}
