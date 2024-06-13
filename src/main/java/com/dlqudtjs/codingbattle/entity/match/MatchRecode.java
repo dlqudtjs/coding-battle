c390fdda7e2fff070ab3637f364246bbed0a1ccd
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-14 02:43:21 +0900

    feat - 매치 정보를 저장하는 클래스 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/entity/match/MatchRecode.java
@@ -0,0 +1,25 @@
+package com.dlqudtjs.codingbattle.entity.match;
+
+import com.dlqudtjs.codingbattle.common.constant.MatchingResultType;
+import com.dlqudtjs.codingbattle.common.constant.ProblemLevelType;
+import com.dlqudtjs.codingbattle.common.constant.ProgrammingLanguage;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.ZonedDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MatchRecode {
+
+    private List<MatchRecodeUserStatus> usersResult;
+    private Long matchId;
+    private ProgrammingLanguage language;
+    private MatchingResultType result;
+    private ProblemLevelType problemLevel;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    private String elapsedTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private ZonedDateTime date;
+}
