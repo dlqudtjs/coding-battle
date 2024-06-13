70737a092819f3369d3ef5470f217de3377b7c50
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-14 02:37:47 +0900

    remove - 상수 테이블 관련 Entity 삭제

--- src/main/java/com/dlqudtjs/codingbattle/repository/game/MatchingResultClassificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dlqudtjs.codingbattle.repository.game;
-
-import com.dlqudtjs.codingbattle.entity.match.MatchingResultClassification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatchingResultClassificationRepository extends
-        JpaRepository<MatchingResultClassification, Long> {
-
-}
--- src/main/java/com/dlqudtjs/codingbattle/repository/problem/ProblemLevelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dlqudtjs.codingbattle.repository.problem;
-
-import com.dlqudtjs.codingbattle.entity.problem.ProblemLevel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemLevelRepository extends JpaRepository<ProblemLevel, Long> {
-    ProblemLevel findByName(String name);
-}
