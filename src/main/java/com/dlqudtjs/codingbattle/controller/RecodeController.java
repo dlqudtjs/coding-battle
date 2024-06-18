3b5e55f43d0e19e45c41fff4165a3ed431d34b9c
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-18 16:44:22 +0900

    refactor - overall 조회 시 pending 값은 반영을 안 하도록 수정

--- src/main/java/com/dlqudtjs/codingbattle/controller/RecodeController.java
+++ src/main/java/com/dlqudtjs/codingbattle/controller/RecodeController.java
@@ -15,6 +15,7 @@ import com.dlqudtjs.codingbattle.service.user.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +50,7 @@ public class RecodeController {
                         .build()).toList();
 
         return ResponseEntity.ok().body(ResponseDto.builder()
+                .status(HttpStatus.OK.value())
                 .data(RecodeListResponseDto.builder()
                         .matchRecodeList(matchRecodes)
                         .currentPage(currentPage)
@@ -64,6 +66,7 @@ public class RecodeController {
         List<ResultCountDto> resultCountDto = matchService.getResultCountByUser(user);
 
         return ResponseEntity.ok().body(ResponseDto.builder()
+                .status(HttpStatus.OK.value())
                 .data(new ResultCountResponseDto(resultCountDto))
                 .build());
     }
--- src/main/java/com/dlqudtjs/codingbattle/dto/recode/ResultCountResponseDto.java
+++ src/main/java/com/dlqudtjs/codingbattle/dto/recode/ResultCountResponseDto.java
@@ -2,7 +2,6 @@ package com.dlqudtjs.codingbattle.dto.recode;
 
 import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.DRAW;
 import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.LOSE;
-import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.PENDING;
 import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.WIM;
 
 import java.util.List;
@@ -12,17 +11,13 @@ import lombok.Getter;
 public class ResultCountResponseDto {
 
     private Long total = 0L;
-    private Long pending = 0L;
     private Long win = 0L;
     private Long lose = 0L;
     private Long draw = 0L;
 
     public ResultCountResponseDto(List<ResultCountDto> resultCounts) {
         resultCounts.forEach(resultCount -> {
-            if (resultCount.getMatchResult().getName().equals(PENDING.getName())) {
-                this.pending = resultCount.getCount();
-                this.total += resultCount.getCount();
-            } else if (resultCount.getMatchResult().getName().equals(WIM.getName())) {
+            if (resultCount.getMatchResult().getName().equals(WIM.getName())) {
                 this.win = resultCount.getCount();
                 this.total += resultCount.getCount();
             } else if (resultCount.getMatchResult().getName().equals(LOSE.getName())) {
