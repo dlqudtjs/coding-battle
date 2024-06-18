828f6ac8c445437390d60bb0cd6e0765e4462c8d
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-18 11:08:13 +0900

    feat : 유저ID로 게임 결과별 count를 조회하는 쿼리 구현

--- src/main/java/com/dlqudtjs/codingbattle/repository/game/UserMatchingHistoryRepository.java
+++ src/main/java/com/dlqudtjs/codingbattle/repository/game/UserMatchingHistoryRepository.java
@@ -1,5 +1,6 @@
 package com.dlqudtjs.codingbattle.repository.game;
 
+import com.dlqudtjs.codingbattle.dto.recode.ResultCountDto;
 import com.dlqudtjs.codingbattle.entity.match.MatchHistory;
 import com.dlqudtjs.codingbattle.entity.match.UserMatchingHistory;
 import java.util.List;
@@ -16,6 +17,12 @@ public interface UserMatchingHistoryRepository extends JpaRepository<UserMatchin
             + "WHERE umh.user.id = :userId AND mh.endTime IS NOT NULL "
             + "ORDER BY mh.startTime DESC")
     Page<MatchHistory> findMatchHistoriesByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT new com.dlqudtjs.codingbattle.dto.recode.ResultCountDto(umh.result, COUNT(umh)) "
+            + "FROM UserMatchingHistory umh "
+            + "WHERE umh.user.id = :userId "
+            + "GROUP BY umh.result")
+    List<ResultCountDto> findResultCountByUserId(@Param("userId") Long userId);
 
     List<UserMatchingHistory> findByMatchHistoryId(Long matchHistoryId);
 
