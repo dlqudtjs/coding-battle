b3dcb2c61215999df39cfc193d5672464794fd63
dlqudtjs <dlqudtjs922@gmail.com>
2024-06-18 11:09:44 +0900

    feat : 결과 별 count를 담는 ResultCountResponseDto 생성

---
+++ src/main/java/com/dlqudtjs/codingbattle/dto/recode/ResultCountResponseDto.java
@@ -0,0 +1,37 @@
+package com.dlqudtjs.codingbattle.dto.recode;
+
+import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.DRAW;
+import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.LOSE;
+import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.PENDING;
+import static com.dlqudtjs.codingbattle.common.constant.MatchResultManager.WIM;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class ResultCountResponseDto {
+
+    private Long total = 0L;
+    private Long pending = 0L;
+    private Long win = 0L;
+    private Long lose = 0L;
+    private Long draw = 0L;
+
+    public ResultCountResponseDto(List<ResultCountDto> resultCounts) {
+        resultCounts.forEach(resultCount -> {
+            if (resultCount.getMatchResult().getName().equals(PENDING.getName())) {
+                this.pending = resultCount.getCount();
+                this.total += resultCount.getCount();
+            } else if (resultCount.getMatchResult().getName().equals(WIM.getName())) {
+                this.win = resultCount.getCount();
+                this.total += resultCount.getCount();
+            } else if (resultCount.getMatchResult().getName().equals(LOSE.getName())) {
+                this.lose = resultCount.getCount();
+                this.total += resultCount.getCount();
+            } else if (resultCount.getMatchResult().getName().equals(DRAW.getName())) {
+                this.draw = resultCount.getCount();
+                this.total += resultCount.getCount();
+            }
+        });
+    }
+}
